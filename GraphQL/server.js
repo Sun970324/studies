d6a03085ee01e0f27db9feb348ec35c186d0c008
Sun970324 <ysw5202222@gmail.com>
2022-10-05 23:52:53 +0900

    study Apollo

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -1,59 +1,113 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
   {
     id: "1",
-    text: "first",
+    text: "first one!",
+    userId: "2",
   },
   {
     id: "2",
-    text: "second",
+    text: "second one",
+    userId: "1",
   },
 ];
+
 let users = [
   {
     id: "1",
-    firstName: "sun",
-    lastName: "yoon",
+    firstName: "nico",
+    lastName: "las",
   },
   {
-    id:"2",
-    firstName:"second",
-    lastName:"name"
-  }
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
+  },
 ];
+
 const typeDefs = gql`
   type User {
     id: ID!
     firstName: String!
     lastName: String!
-    fullName:String!
+    """
+    Is the sum of firstName + lastName as a string
+    """
+    fullName: String!
   }
+  """
+  Tweet object represents a resource for  a Tweet
+  """
   type Tweet {
-    id: ID
-    text: String
+    id: ID!
+    text: String!
     author: User
   }
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
-    allTweets: [Tweet]!
+    allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
   type Mutation {
-    postTweet(text: String!, userId: ID!): Tweet
+    postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
   }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+  }
 `;
+// GET /api/v1/tweets
+// POST DELETE PUT /api/v1/tweets
+// GET /api/v1/tweet/:id
+
 const resolvers = {
   Query: {
-    tweet(root, { id }) {
-      return tweets.find((tweet) => tweet.id === id);
-    },
     allTweets() {
       return tweets;
     },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
     allUsers() {
+      console.log("allUsers called!");
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
     },
   },
   Mutation: {
@@ -61,6 +115,7 @@ const resolvers = {
       const newTweet = {
         id: tweets.length + 1,
         text,
+        userId,
       };
       tweets.push(newTweet);
       return newTweet;
@@ -73,11 +128,17 @@ const resolvers = {
     },
   },
   User: {
-    fullName({firstName, lastName}) {
-      return `${firstName} ${lastName}`
-    }
-  }
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
+    },
+  },
 };
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
