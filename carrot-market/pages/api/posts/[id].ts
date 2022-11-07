8f3e63744388f1269a6e658aab0e90592d56ab74
Sun970324 <ysw5202222@gmail.com>
2022-11-07 15:09:14 +0900

    Post Form, Handler

---
+++ carrot-market/pages/api/posts/[id].ts
@@ -0,0 +1,53 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+
+  const post = await client.post.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answer: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answer: true,
+          wonderings: true,
+        },
+      },
+    },
+  });
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
